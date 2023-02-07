3830b23f80c3295a631080c024f188ad9847aa73
mackay <johnmackay150@gmail.com>
2023-02-07 17:34:06 +0900

    ADD:interactive next data fetch

--- src/features/mics/Root.tsx
+++ src/features/mics/Root.tsx
@@ -1,21 +1,22 @@
-import { atom, useAtom } from 'jotai';
+import { Button } from '@chakra-ui/react';
+import { useAtom } from 'jotai';
 import { atomsWithInfiniteQuery } from 'jotai-tanstack-query';
 
-const idAtom = atom(3);
-const [userAtom] = atomsWithInfiniteQuery(get => ({
-  queryKey: ['users', get(idAtom)],
-  queryFn: async ({ queryKey: [, id] }) => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-    return res.json();
+const [postsAtom] = atomsWithInfiniteQuery(() => ({
+  queryKey: ['posts'],
+  queryFn: async ({ pageParam = 1 }) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${pageParam}`);
+    const data: { id: number; title: string } = await res.json();
+    return data;
   },
-  // infinite queries can support paginated fetching
-  getNextPageParam: (lastPage, pages) => lastPage.nextCursor,
+  getNextPageParam: lastPage => lastPage.id + 1,
 }));
-
 export const Root = () => {
-  const [data] = useAtom(userAtom);
+  const [data, dispatch] = useAtom(postsAtom);
+  const handleFetchNextPage = () => dispatch({ type: 'fetchNextPage' });
   return (
     <>
+      <Button onClick={handleFetchNextPage}>Next</Button>
       {data.pages.map((userData, index) => (
         <div key={index}>{JSON.stringify(userData)}</div>
       ))}
